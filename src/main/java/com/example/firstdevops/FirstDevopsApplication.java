5ce4b2d0dd33ca41fa25e67adf667aa567b75d0b
rydtrg <6304455@cegepmv.ca>
2026-01-11 00:29:56 -0500

    hello world

---
+++ src/main/java/com/example/firstdevops/FirstDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstDevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstDevopsApplication.class, args);
+    }
+
+}
